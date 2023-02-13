b9a41acfe1488d5d8c1b30987ee7bd6d868b9d87
Daniele420 <d.aleksandrov2018@pmg-pernik.com>
2023-02-13 13:49:15 +0200

    Baza Danni

---
+++ DiplomenProektNo7/DiplomenProektNo7/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using DiplomenProektNo7.Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiplomenProektNo7.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            this.Database.EnsureCreated();
+        }
+        public DbSet<Shoe> Shoes { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
